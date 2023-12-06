0aeb49ef2eb87b9b92fb0253239d252efa009fcf
go2023 <tetyanago2018@gmail.com>
2023-12-05 17:32:39 -0800

    reset_controller, scroll_controller were created and bound

---
+++ app/javascript/controllers/scroll_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+    /** On start */
+    connect() {
+        console.log("Connected");
+        const messages = document.getElementById("messages");
+        // messages.addEventListener("DOMNodeInserted", this.resetScroll);
+        messages.addEventListener("MutationEvent", this.resetScroll);
+        this.resetScroll(messages);
+    }
+    /** On stop */
+    disconnect() {
+        console.log("Disconnected");
+    }
+    /** Custom function */
+    resetScroll() {
+        messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+    }
+}
